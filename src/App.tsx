47a828031fda9e1c3fd7dea0200531791dd3251e
Marek <marek@ilieth.at>
2023-08-03 11:36:52 +0200

    feat: add stepper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
+import Stepper from './stepper';
+
 function App() {
+    const [activeStep, setActiveStep] = useState<number>(0);
+
+    useEffect(() => {
+        setActiveStep(0);
+    }, []);
+
     return (
         <div className="App">
-            <></>
+            <>
+                <Stepper activeStep={activeStep} />
+            </>
         </div>
     );
 }
